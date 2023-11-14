22d098dbc7bd125c68fb671db13c05529f19d6ef
rodio <rodionmarkelov01229@gmail.com>
2023-11-14 23:46:17 +0400

    basic class

--- node_lab/node_lab.cpp
+++ node_lab/node_lab.cpp
@@ -7,14 +7,13 @@ using namespace std;
 
 int main()
 {
-	SingleLinkedList<int> l1;
-	SingleLinkedList<int> l2 = l1;
-	l1.push_tail(3);
-	l1.push_tail(4);
-	l1.push_tail(-3);
-
-	cout << l1.get_size() << endl;
-	cout << l2.get_size() << endl;
+	cout << "push_head" << endl;
+	SingleLinkedList<int> l3(10, 10, 100);
+	l3.print();
+	SingleLinkedList<float> l2(10, 10.0f, 100.0f);
+	l2.print();
+	SingleLinkedList<double> l1(10, 0.0, 100.0);
 	l1.print();
+
 	return 0;
 }
--- node_lab/node_lab.h
+++ node_lab/node_lab.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <random>
 #include <stdexcept>
+#include <ctime>
 
 using namespace std;
 
@@ -22,75 +23,224 @@ class SingleLinkedList {
 	Node<T>* _head;
 public:
 	SingleLinkedList() : _size(0), _head(nullptr) {};
-	SingleLinkedList(const SingleLinkedList<T>& other) : _size(other._size), _head(other._head) {};
+	SingleLinkedList(const SingleLinkedList<T>& other) {
+		_head = nullptr;
+		_size = 0;
+		if (other._head) {
+			Node<T>* current = other._head;
+			do {
+				push_tail(current->_data);
+				current = current->_next;
+			} while (current != other._head);
+		}
+	}
 
-	int get_size() {
+	SingleLinkedList(int size,const T min_value,const T max_value) : _head(nullptr) {
+		if (size < 0) {
+			throw out_of_range("Size can't be < 0.");
+		}
+		
+		for (int i = 0; i < size; ++i) {
+			T value = (T)(1 + (double)(rand()) / RAND_MAX * (max_value - min_value + 1));
+			push_tail(value);
+		}
+	}
+
+	~SingleLinkedList() {
+		while (_head) {
+			pop_head();
+		}
+	}
+
+	int get_size() const {
 		return _size;
 	}
 
 	SingleLinkedList<T> operator=(const SingleLinkedList<T> other) {
-		if (_size != other._size) {
-			throw runtime_error("[SingleLinkedList<T>::operator=] Different size.");
+		if (other._head) {
+			Node<T>* othercurrent = other._head;
+			do {
+				push_tail(othercurrent->_cofficient);
+				othercurrent = othercurrent->_next;
+			} while (othercurrent != other._head);
 		}
-		SingleLinkedList<T> copy(other);
-		swap(copy);
 		return *this;
-	}
-
-	void swap(SingleLinkedList<T>& other) noexcept {
-		std:: swap(_size, other._size);
-		std::swap(_head, other._head);
 	}
 
 	void push_tail(T value) {
 		Node<T>* element = new Node<T>(value);
-		if (!_head) {
+		if (_head == nullptr) {
 			_head = element;
 			_head->_next = _head;
 		}
 		else {
 			Node<T>* current = _head;
-			while (current->_next) {
-				current->_next;
+			while (current->_next != _head) {
+				current = current->_next;
 			}
 			current->_next = element;
 			element->_next = _head;
 		}
+		_size++;
 	}
-	
-	void pop_tail() {
-		if (_head) {
+
+	void push_tail(SingleLinkedList<T> other) {
+		if (other._head == nullptr) {
+			return;
+		}
+		else {
+			Node<T>* current = other._head;
+			do {
+				push_tail(current->_data);
+				current = current->_next;
+			} while (current != other._head);
+		}
+	}
+
+	void push_head(T value) {
+		Node<T>* element = new Node<T>(value);
+		if (_head == nullptr) {
+			_head = element;
+			_head->_next = _head;
+			_size++;
+			return;
+		}
+		Node<T>* current = _head;
+		while (current->_next != _head) {
+			current = current->_next;
+		}
+		element->_next = _head;
+		current->_next = element;
+		_head = element;
+		_size++;
+	}
+
+	void push_head(SingleLinkedList<T>& other) {
+		Node<T>* othercurrent = other._head;
+		if (othercurrent) {
+			do {
+				push_head(othercurrent->_data);
+				othercurrent = othercurrent->_next;
+			} while (othercurrent != other._head);
+		}
+	}
+
+	void pop_head() {
+		if (_size == 0) {
+			cout << "List is empty." << endl;
+			return;
+		}
+		if (_head && _head->_next == _head) {
+			delete _head;
+			_head = nullptr;
+		}
+		else {
+			Node<T>* temp = _head;
 			Node<T>* current = _head;
-			Node<T>* prev = nullptr;
-			while (current->_next) {
-				prev = current;
-				current->_next;
+			while (current->_next != _head) {
+				current = current->_next;
 			}
-			if (prev) {
-				prev->_next = _head;
+			_head = _head->_next;
+			current->_next = _head;
+			delete temp;
+		}
+		_size--;
+	}
+
+	void pop_tail() {
+		if (_size == 0) {
+			cout << "List is empty." << endl;
+			return;
+		}
+		Node<T>* current = _head;
+		Node<T>* penultimate = nullptr;
+		while (current->_next != _head) {
+			penultimate = current;
+			current = current->_next;
+		}
+		if (penultimate) {
+			penultimate->_next = _head;
+		}
+		else {
+			_head = nullptr;
+		}
+		delete current;
+		_size--;
+
+	}
+
+	void delete_node(const Node<T>& node) {
+		if (get_size() == 0) {
+			return;
+		}
+		if (_head != nullptr) {
+			Node<T>* current = _head;
+			while (current->_next != _head) {
+				if (node._data == current->_next->_data) {
+					Node<T>* temp = current->_next;
+					current->_next = current->_next->_next;
+					delete temp;
+					_size--;
+				}
+				else current = current->_next;
 			}
-			else {
-				_head = nullptr;
+		}
+		if (_head && _head->_data == node._data) {
+			pop_head();
+		}
+	}
+
+	T operator[](const int index) const {
+		if (index < 0 || index >= get_size()) {
+			throw runtime_error("[operator[]]: Index out of range.");
+		}
+		if (get_size() == 0) {
+			throw runtime_error("[operator[]]: List is empty.");
+		}
+		int counter = 0;
+		Node<T>* current = _head;
+		while (current) {
+			if (counter == index) {
+				return current->_data;
 			}
-			delete current;
+			current = current->_next;
+			counter++;
+		}
+	}
+
+
+	T& operator[](const int index) {
+		if (index < 0 || index >= get_size()) {
+			throw runtime_error("[operator[]]: Index out of range.");
+		}
+		if (get_size() == 0) {
+			throw runtime_error("[operator[]]: List is empty.");
+		}
+		int counter = 0;
+		Node<T>* current = _head;
+		while (current) {
+			if (counter == index) {
+				return current->_data;
+			}
+			current = current->_next;
+			counter++;
 		}
 	}
 
 	void print() {
-		if (!_head) {
+		if (_head == nullptr) {
 			std::cout << "List is empty." << endl;
+			return;
 		}
 		Node<T>* current = _head;
 		if (current->_next == _head) {
-			cout << "List:" << current->_data << endl;
+			cout << current->_data << endl;
+			return;
 		}
 		while (current->_next != _head) {
-			cout << current->_data << endl;
+			cout << current->_data << ", ";
 			current = current->_next;
 		}
-		
-		~SingleLinkedList() {
-
-		}
+		cout << current->_data << endl;
 	}
 };
